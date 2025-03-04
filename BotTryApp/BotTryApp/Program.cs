eab02f289b6501c7200f8ccb1b3e220162dffb9d
urmasa <nik.goltsev.97@bk.ru>
2025-03-04 03:17:44 +0300

    Started_bot

---
+++ BotTryApp/BotTryApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+namespace BotTryApp
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new jsonReader();
+            await jsonReader.ReadJSON();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordConfig);
+
+            Client.Ready += Client_Ready;
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ BotTryApp/BotTryApp/jsonReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BotTryApp
+{
+    internal class jsonReader
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+
+        public async Task ReadJSON()
+        {
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+                JSONstructer data = JsonConvert.DeserializeObject<JSONstructer>(json);
+
+                this.token = data.token;
+                this.prefix = data.prefix;
+
+            }
+        }
+    }
+
+    internal sealed class JSONstructer
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+}
